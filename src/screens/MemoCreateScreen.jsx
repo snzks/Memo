ae70fd6b086ee35200a5e09d712f35d1f9374170
snzks <shun.1030.rrss@gmail.com>
2022-07-18 08:48:50 +0900

    Save memo on firestore

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,42 +1,68 @@
-import React from "react";
-import { 
-    View, Text, TextInput, StyleSheet , KeyboardAvoidingView, Keyboard
+import React, { useState } from "react";
+import {
+    View, Text, TextInput, StyleSheet, KeyboardAvoidingView, Keyboard, Alert
 } from "react-native";
 import AppBar from "../components/AppBar";
 import CircleButton from "../components/CircleButton";
 import KeyboardSafeView from "../components/KeyboardSafeView";
+import firebase from "firebase";
 
-export default function MemoCreateScreen(props){
-    const {navigation} = props;
-    return(
+export default function MemoCreateScreen(props) {
+    const [bodyText, setBodyText] = useState('');
+    const { navigation } = props;
+    function handlePress() {
+        const { currentUser } = firebase.auth();
+        const db = firebase.firestore();
+        const ref = db.collection(`users/${currentUser.uid}/memos`);
+        ref.add({
+            bodyText,
+            updaatedAt: new Date(),
+        })
+            .then((docRef) => {
+                Alert.alert(docRef.id);
+            })
+            .catch((error) => {
+                Alert.alert(error);
+            })
+        navigation.goBack();
+    }
+
+    return (
         <KeyboardSafeView style={styles.container}>
-            
+
             <View style={styles.inputContainer}>
-                <TextInput Value="" multiline style={styles.input} onSubmitEditing={Keyboard.dismiss}></TextInput>
+                <TextInput
+                    Value={bodyText}
+                    multiline
+                    style={styles.input}
+                    onSubmitEditing={Keyboard.dismiss}
+                    onChangeText={(text)=>{setBodyText(text);}}
+                    autoFocus
+                    />
             </View>
             <CircleButton
-            name="check"
-            onPress={()=>{navigation.goBack(); }}
+                name="check"
+                onPress={handlePress}
             />
         </KeyboardSafeView>
     );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
     },
-    inputContainer:{
-        flex:1,
-        paddingHorizontal:27,
-        paddingVertical:32,
+    inputContainer: {
+        flex: 1,
+        paddingHorizontal: 27,
+        paddingVertical: 32,
     },
-    input:{
-        flex:1,
-        textAlignVertical:"top",
-        fontSize:16,
-        lineHeight:24,
-        color:"#000000",
+    input: {
+        flex: 1,
+        textAlignVertical: "top",
+        fontSize: 16,
+        lineHeight: 24,
+        color: "#000000",
     }
 
 })
